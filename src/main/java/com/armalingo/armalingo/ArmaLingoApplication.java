4807ea074218a7f90ceb8ea6052b448b811d4cde
Artem Minich <temic543.9@gmail.com>
2024-02-12 12:03:40 +0200

    Initial commit

---
+++ src/main/java/com/armalingo/armalingo/ArmaLingoApplication.java
@@ -0,0 +1,13 @@
+package com.armalingo.armalingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmaLingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmaLingoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/armalingo/armalingo/ArmaLingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armalingo.armalingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmaLingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
