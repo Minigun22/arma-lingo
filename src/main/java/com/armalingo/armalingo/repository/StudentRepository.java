341c66e5d0f50d174598fe05aa4fc19c55fdd7a0
Artem Minich <temic543.9@gmail.com>
2024-02-12 22:40:24 +0200

    add simple repository for student

---
+++ src/main/java/com/armalingo/armalingo/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.armalingo.armalingo.repository;
+
+import com.armalingo.armalingo.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
