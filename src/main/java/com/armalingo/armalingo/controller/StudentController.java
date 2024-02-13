755424c59549647473bbba2b7f77ef89f590b35e
Jokail <jokailxx@gmail.com>
2024-02-13 17:10:48 +0200

    add getUserById in StudentController

--- src/main/java/com/armalingo/armalingo/controller/StudentController.java
+++ src/main/java/com/armalingo/armalingo/controller/StudentController.java
@@ -2,15 +2,14 @@ package com.armalingo.armalingo.controller;
 
 import com.armalingo.armalingo.model.Student;
 import com.armalingo.armalingo.repository.StudentRepository;
+import com.armalingo.armalingo.util.Roles;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Controller
@@ -26,10 +25,26 @@ public class StudentController {
 
     @PostMapping("/registration")
     public ResponseEntity<Student> registerUser(@RequestBody Student student){
+        student.setRole(Roles.ROLE_USER);
         Student saveStudent = studentRepository.save(student);
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(saveStudent.getId()).toUri());
+        httpHeaders.setLocation(ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(saveStudent.getId())
+                .toUri());
         return new ResponseEntity<>(saveStudent, httpHeaders, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getUserById(@PathVariable("id") Long id){
+        Student student;
+        try {
+            student = studentRepository.getReferenceById(id);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
     }
 
 }
