8f40c3bad96d0c5a429daf16d06592d90393bccb
Jokail <jokailxx@gmail.com>
2024-02-15 23:12:59 +0200

    delete CSRFController and create MainController

--- src/main/java/com/armalingo/armalingo/controller/MainController.java
+++ src/main/java/com/armalingo/armalingo/controller/MainController.java
@@ -1,4 +1,16 @@
 package com.armalingo.armalingo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+
+    @GetMapping("/welcome/select-language")
+    public String selectLanguagePage(){
+        return "welcome/select-language";
+    }
+
+
+
 }
