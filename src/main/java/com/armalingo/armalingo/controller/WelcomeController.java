68002182e60da98e8b54a5d8fdbc2710b1e75ff0
Jokail <jokailxx@gmail.com>
2024-02-16 16:43:18 +0200

    create WelcomeController and registration.html

---
+++ src/main/java/com/armalingo/armalingo/controller/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.armalingo.armalingo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping("/select-language")
+    public String selectLanguagePage(){
+        return "welcome/select-language";
+    }
+
+    @GetMapping("/select-role")
+    public String selectRolePage(){ return  "welcome/select-role";}
+
+}
